1ab6531390ae1417f2e22edb17f53dfed9a7c430
Björn Leví <bjornlevi@gmail.com>
2015-04-27 12:16:37 +0000

    basic list of matsviðmið on the page

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+function getVars() {
+    var vars = [], hash;
+    var q = document.URL.split('?')[1];
+    if (q != undefined) {
+        q = q.split('&');
+        for (var i = 0; i < q.length; i++) {
+            hash = q[i].split('=');
+            vars.push(hash[1]);
+            vars[hash[0]] = hash[1];
+        }
+    }
+    return vars;
+}
+
+function display_matsvidmid(matsvidmid)
+{
+	$.each(matsvidmid, function (item)
+	{
+		$("#matsvidmid_list").append('<li data-item="' + item + '"><span class="glyphicon glyphicon-plus">' + item + '</li>');
+	});
+}
 
 function load_matsvidmid(matsvidmid)
 {
-	console.log(matsvidmid);
+	display_matsvidmid(matsvidmid);
 }
 
 // Inject the JSONp "script" from the location defined in the URL.
@@ -11,9 +32,9 @@ $.ajax({
     dataType: 'script',
     timeout: 5000,
     success: function () {
-        load_matsvidmid(matsvidmid);
+        load_matsvidmid(matsvidmid_json);
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
-        alert("Failed loading data - " + textStatus + " - " + errorThrown);
+        console.log("Failed loading data - " + textStatus + " - " + errorThrown);
     }
 });
